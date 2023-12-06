dcb67bfa040de9c844b28551c484399132db948c
Snow-EWhitt <ethanwhittaker02@gmail.com>
2023-12-06 12:51:21 -0700

    Created API with websockets

--- api/Program.cs
+++ api/Program.cs
@@ -1,3 +1,5 @@
+using System.Net.WebSockets;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -12,8 +14,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+  app.UseSwagger();
+  app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
@@ -21,5 +23,23 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseWebSockets();
+
+app.Map("/ws/repeater", async (context) =>
+{
+  if (context.WebSockets.IsWebSocketRequest)
+  {
+    var webSocketManager = app.Services.GetRequiredService<WebSocketManager>();
+    WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync();
+    string connId = webSocketManager.AddSocket(webSocket);
+    await webSocketManager.EchoMessagesAsync(webSocket, CancellationToken.None);
+  }
+  else
+  {
+    context.Response.StatusCode = StatusCodes.Status400BadRequest;
+    await context.Response.WriteAsync("Not a websocket request");
+  }
+});
 
 app.Run();
